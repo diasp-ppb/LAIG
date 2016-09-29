5ee7fb774f58f723b75e1d41be65b5a4bd9f1f82
Pedro Dias <Pedro Dias>
2016-09-30 00:13:30 +0100

    parser Ligths/textures started transformations

--- reader/MySceneGraph.js
+++ reader/MySceneGraph.js
@@ -16,9 +16,17 @@ function MySceneGraph(filename, scene) {
 
     this.reader.open('scenes/' + filename, this);
 
-    /* Stored scene perpectives*/
+    /* Storage for scene perpectives*/
     this.perspectives = [];
-
+    /* Storage for scene lights*/
+    this.lights = {
+        omnis: [],
+        spots: []
+    };
+    /* Storage for textures information*/
+    this.textures = [];
+    /* Storage for materials*/
+    this.materials = [];
 }
 
 /*
@@ -31,6 +39,9 @@ MySceneGraph.prototype.onXMLReady = function() {
     // Here should go the calls for different functions to parse the various blocks
     var error = this.parserIllumination(rootElement);
     error = this.parserViews(rootElement); //TODO
+    error = this.parserLights(rootElement);
+    error = this.parserTextures(rootElement);
+    error = this.parserMaterials(rootElement);
     if (error != null) {
         this.onXMLError(error);
         return;
@@ -86,20 +97,7 @@ MySceneGraph.prototype.parseGlobalsExample = function(rootElement) {
 
 };
 
-MySceneGraph.prototype.parserPrimitives = function(rootElement) {
-    var primitives = rootElement.getElementsByTagName('primitives');
 
-    if (primitives == null || primitives.length == 0) {
-        return "primitives are missing";
-    }
-
-    var nnodes = primitives[0].childen.length;
-    if (nnodes <= 0) {
-        return "wrong number of basic objects";
-    }
-
-    /** TODO */
-};
 MySceneGraph.prototype.parserViews = function(rootElement) {
     var views = rootElement.getElementsByTagName('views');
 
@@ -114,19 +112,18 @@ MySceneGraph.prototype.parserViews = function(rootElement) {
     if (nnodes <= 0) {
         return 'no perspectives on file';
     }
-
+    var child;
     for (var i = 0; i < nnodes; i++) {
-        var child = views[0].children[i];
+        child = views[0].children[i];
         if (child.nodeName === "perspective") {
             var perspective = {
-                id: child.getAttribute("id"),
-                near: child.getAttribute("near"),
-                far: child.getAttribute("far"),
-                angle: child.getAttribute("angle"),
-                from: {},
-                to: {}
+                id: this.reader.getString(child, "id", 1),
+                near: this.reader.getFloat(child, "near", 1),
+                far: this.reader.getFloat(child, "far", 1),
+                angle: this.reader.getFloat(child, "angle", 1),
+                from:  [],
+                to:  []
             };
-
             var childNodes = child.children.length;
             if (childNodes < 2)
                 return "wrong number of perspective " + perspective.id + "children";
@@ -135,14 +132,14 @@ MySceneGraph.prototype.parserViews = function(rootElement) {
             for (var k = 0; k < childNodes; k++) {
                 childSon = child.children[k];
                 if (childSon.nodeName !== "from" && childSon.nodeName !== "to") {
-                  return "invalid perspective " + perspective.id + " son ";}
-
-                    perspective[childSon.nodeName] = {
-                        x: childSon.getAttribute("x"),
-                        y: childSon.getAttribute("y"),
-                        z: childSon.getAttribute("z")
-                    };
-                  }
+                    return "invalid perspective " + perspective.id + " son ";
+                }
+                perspective[childSon.nodeName] = {
+                    x: this.reader.getFloat(childSon, "x", 1),
+                    y: this.reader.getFloat(childSon, "y", 1),
+                    z: this.reader.getFloat(childSon, "z", 1),
+                }
+            }
         }
         this.perspectives.push(perspective);
     }
@@ -166,29 +163,202 @@ MySceneGraph.prototype.parserIllumination = function(rootElement) {
     if (nnodes != 2) {
         return "'illumination' wrong number of children ";
     }
-
+    var child;
     for (var i = 0; i < nnodes; i++) {
-        var child = ilumi[0].children[i];
+        child = ilumi[0].children[i];
         if (child.nodeName === "ambient") {
-            this.scene.setAmbient(child.getAttribute("r"), child.getAttribute("g"), child.getAttribute("b"), child.getAttribute("a"));
+            this.scene.setAmbient(this.reader.getFloat(child, "r", 1), this.reader.getFloat(child, "g", 1), this.reader.getFloat(child, "b", 1), this.reader.getFloat(child, "a", 1));
             // TODO FALTA TESTAR ISTO PRECISO Objectos
         } else if (child.nodeName === "background") {
-            this.background = child.getAttribute("r") + child.getAttribute("g") + child.getAttribute("b") + child.getAttribute("a");
+            this.background = this.reader.getFloat(child, "r", 1) + "" +
+                this.reader.getFloat(child, "g", 1) + "" +
+                this.reader.getFloat(child, "b", 1) + "" +
+                this.reader.getFloat(child, "a", 1);
         }
+
     }
 };
 
-MySceneGraph.prototype.parserLights = fuction(rootElement) {
-  var lights = rootElement.getElementsByTagName('lights');
+MySceneGraph.prototype.parserLights = function(rootElement) {
+    var lights = rootElement.getElementsByTagName('lights');
 
-  if (lights == null ||  lights.length == 0) {
-      return "'lights' is missing";
+    if (lights == null || lights.length == 0) {
+        return "'lights' is missing";
+    }
+
+    var nnodes = lights[0].children.length;
+    if (nnodes < 1) {
+        return "wrong number of lights children";
+    }
+
+    var child;
+    for (var i = 0; i < nnodes; i++) {
+        child = lights[0].children[i];
+        if (child.nodeName === "omni") {
+            var nodesSon = child.children.length;
+            var omni = {
+                id: this.reader.getString(child, "id", 1),
+                enabled: this.reader.getBoolean(child, "enabled", 1),
+                location:  [],
+                ambient:  [],
+                diffuse: [],
+                specular: []
+            };
+
+            var childSon;
+            for (var k = 0; k < nodesSon; k++) {
+                childSon = child.children[k];
+
+                if (childSon.nodeName === "location") {
+                    omni.location = {
+                        x: this.reader.getFloat(childSon, "x", 1),
+                        y: this.reader.getFloat(childSon, "y", 1),
+                        z: this.reader.getFloat(childSon, "z", 1),
+                        w: this.reader.getFloat(childSon, "w", 1)
+                    }
+                } else if (childSon.nodeName === "ambient" || childSon.nodeName === "diffuse" ||
+                    childSon.nodeName === "specular") {
+                    omni[childSon.nodeName] = {
+                        r: this.reader.getFloat(childSon, "r", 1),
+                        g: this.reader.getFloat(childSon, "g", 1),
+                        b: this.reader.getFloat(childSon, "b", 1),
+                        a: this.reader.getFloat(childSon, "a", 1)
+                    }
+                }
+            }
+            this.lights.omnis.push(omni);
+        } else if (child.nodeName === "spot") {
+            var spot = {
+                id: this.reader.getString(child, "id", 1),
+                enabled: this.reader.getBoolean(child, "enabled", 1),
+                angle: this.reader.getFloat(child, "angle", 1),
+                exponent: this.reader.getFloat(child, "exponent", 1),
+                target: [],
+                location: [],
+                ambient:  [],
+                diffuse:  [],
+                specular: []
+            };
+
+
+            var childSon;
+            for (var k = 0; k < nodesSon; k++) {
+                childSon = child.children[k];
+                if (childSon.nodeName === "target" || childSon.nodeName === "location") {
+                    spot[childSon.nodeName] = {
+                        x: this.reader.getFloat(childSon, "x", 1),
+                        y: this.reader.getFloat(childSon, "y", 1),
+                        z: this.reader.getFloat(childSon, "z", 1),
+
+
+                    };
+                } else {
+                    spot[childSon.nodeName] = {
+                        r: this.reader.getFloat(childSon, "r", 1),
+                        g: this.reader.getFloat(childSon, "g", 1),
+                        b: this.reader.getFloat(childSon, "b", 1),
+                        a: this.reader.getFloat(childSon, "a", 1)
+                    }
+
+
+                }
+            }
+            this.lights.spots.push(spot);
+        }
+    }
+
+};
+
+MySceneGraph.prototype.parserTextures = function(rootElement) {
+  var textures = rootElement.getElementsByTagName('textures');
+
+  if (textures == null || textures.length == 0) {
+      return "'textures' are missing";
+  }
+  var nnodes = textures[0].children.length;
+  if (nnodes < 1) {
+      return "no textures";
   }
 
-  var nnodes=
+  var child;
 
+  for(var i = 0; i < nnodes; i++)
+  {
+    child = textures[0].children[i];
+
+    var texture = {
+      id: this.reader.getString(child, "id", 1),
+      file: this.reader.getString(child, "file", 1),
+      length_s:  this.reader.getFloat(child, "length_s", 1),
+      length_t:  this.reader.getFloat(child, "length_t", 1),
+    }
+    this.textures.push(texture);
+  }
 }
 
+MySceneGraph.prototype.parserMaterials = function(rootElement) {
+  var materials = rootElement.getElementsByTagName('textures');
+
+  if (materials == null || materials.length == 0) {
+      return "'textures' are missing";
+  }
+  var nnodes = materials[0].children.length;
+  if (nnodes < 1) {
+      return "no materials";
+  }
+
+  var child;
+
+  for(var i = 0; i < nnodes; i++){
+    child = materials[0].children[i];
+    var material = {
+      id: this.reader.getString(child, "id", 1),
+      emission: [],
+      ambient: [],
+      diffuse: [],
+      specular: []
+    };
+    var nodesSon = child.children.length;
+
+    var childSon;
+    for(var k = 0; k < nodesSon; k++){
+      childSon=child[0].children[k];
+      if(childSon.nodeName ==="shininess"){
+        material.shininess = this.reader.getFloat(childSon, "value", 1);
+      }
+      else {
+        material[childSon.nodeName] = {
+        r: this.reader.getFloat(childSon, "r", 1),
+        g: this.reader.getFloat(childSon, "g", 1),
+        b: this.reader.getFloat(childSon, "b", 1),
+        a: this.reader.getFloat(childSon, "a", 1)
+        }
+      }
+    }
+this.materials.push(material);
+  }
+}
+
+
+MySceneGraph.prototype.parserTransformations = function(rootElement) {
+  var transformations = rootElement.getElementsByTagName('textures');
+
+  if (transformations == null || transformations.length == 0) {
+      return "'textures' are missing";
+  }
+  var nnodes = transformations[0].children.length;
+  if (nnodes < 1) {
+      return "no transformations";
+  }
+
+  var child;
+
+  for(var i = 0; i < nnodes; i++)
+  {
+    
+  }
+
+}
 /*
  * Callback to be executed on any read error
  */
