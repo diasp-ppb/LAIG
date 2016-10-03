f906df55abe4f2856bc8719de8de4164ec037d3f
rodavoce <up201404178@fe.up.pt>
2016-10-03 12:55:58 +0100

    corrections

--- reader/xmlPrimitives.js
+++ reader/xmlPrimitives.js
@@ -1,17 +1,11 @@
-/**
-* Class that represents a primitive (rectangle, triangle, cylinder, sphere, torus)
-*/
-function xmlPrimitive()
-{
-  this.id = null;
-};
+
 
 /**
 * Class that represents a rectangle primitive
 */
 function xmlRectangle(id, x1, x2, y1, y2)
 {
-  xmlPrimitive.call(this);
+
   this.id = id;
   this.x1 = x1;
   this.x2 = x2;
